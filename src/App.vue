2a630bf6da167a390cbb66210e14587178a400b8
hgyn99 <hyms30329@gmail.com>
2025-04-07 16:02:41 +0900

    chore: 폰트 적용

--- src/App.vue
+++ src/App.vue
@@ -10,39 +10,4 @@ export default {
 };
 </script>
 
-<style>
-@font-face {
-  font-family: 'SpoqaHanSansNeo';
-  src: url('@/assets/fonts/SpoqaHanSansNeo-Regular.ttf') format('trueType');
-  font-weight: 400;
-}
-@font-face {
-  font-family: 'SpoqaHanSansNeo';
-  src: url('@/assets/fonts/SpoqaHanSansNeo-Bold.ttf') format('trueType');
-  font-weight: 700;
-}
-@font-face {
-  font-family: 'SpoqaHanSansNeo';
-  src: url('@/assets/fonts/SpoqaHanSansNeo-Light.ttf') format('trueType');
-  font-weight: 300;
-}
-@font-face {
-  font-family: 'SpoqaHanSansNeo';
-  src: url('@/assets/fonts/SpoqaHanSansNeo-Thin.ttf') format('trueType');
-  font-weight: 100;
-}
-@font-face {
-  font-family: 'SpoqaHanSansNeo';
-  src: url('@/assets/fonts/SpoqaHanSansNeo-Medium.ttf') format('trueType');
-  font-weight: 500;
-}
-
-html,
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'SpoqaHanSansNeo';
-  font-weight: 400;
-  font-size: 30px;
-}
-</style>
+<style></style>
