6f3fec4de0f3143f5445d1c30a15f81f1711d6eb
YeonWoo choi <rryunn@naver.com>
2025-04-09 13:50:48 +0900

    fix : 거래내역 헤더 디자인 수정

--- src/pages/TotalTransaction.vue
+++ src/pages/TotalTransaction.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="total-category">
     <header class="header">
-      <span class="back" @click="goBack">&larr;</span>
+      <!-- 뒤로가기 SVG 버튼 -->
+      <button @click="goBack" class="back-button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="back-icon"
+        >
+          <path
+            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+          />
+        </svg>
+      </button>
       거래 내역
     </header>
 
