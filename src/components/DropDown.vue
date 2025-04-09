5c3326603c46918592ee6b3571c47a6e7fb2e829
chominju <cmj021002@knu.ac.kr>
2025-04-09 11:16:30 +0900

    feat: transaction common component

---
+++ src/components/DropDown.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  label: String,
+  options: Array,
+  selected: String,
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const selectedValue = computed({
+  get: () => props.selected,
+  set: (val) => emit('update:selected', val),
+});
+</script>
+
+<template>
+  <div class="select-wrapper">
+    <label class="label">{{ props.label }}</label>
+    <select v-model="selectedValue" class="dropdown">
+      <option disabled value="">선택하세요</option>
+      <option v-for="option in options" :key="option" :value="option">
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+.select-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+}
+
+.label {
+  font-size: 14px;
+  color: #374151;
+  font-weight: bold;
+}
+
+.dropdown {
+  padding: 10px;
+  border: 2px solid #d1d5db;
+  border-radius: 12px;
+  font-size: 14px;
+}
+</style>
