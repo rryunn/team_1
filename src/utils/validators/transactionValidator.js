42545fb44c01bee711d9878a55d60344c696efe7
chominju <cmj021002@knu.ac.kr>
2025-04-09 11:18:20 +0900

    feat: input validator

---
+++ src/utils/validators/transactionValidator.js
@@ -0,0 +1,16 @@
+// src/utils/validators/transactionValidator.ts
+export function validateTransactionPayload(payload) {
+  const requiredFields = ['title', 'type', 'amount', 'date', 'category'];
+
+  for (const field of requiredFields) {
+    if (!payload[field] || payload[field].toString().trim() === '') {
+      throw new Error(`필수 항목 누락`);
+    }
+  }
+
+  if (isNaN(Number(payload.amount))) {
+    throw new Error('금액은 숫자여야 합니다.');
+  }
+
+  return true;
+}
